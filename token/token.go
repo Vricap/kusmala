51bd0980d57ab903e90c388bd20b42ddb7d30e0b
Vricap <vricagedepenggalih@gmail.com>
2024-11-28 16:55:07 +0700

    define the tokens

--- token/token.go
+++ token/token.go
@@ -1,7 +1,29 @@
 package token
 
-import "fmt"
+type TokenType string // its actually more performant using byte or int
 
-func Test() {
-	fmt.Println("Helo from test")
+// define all token the language will have
+const (
+	ILLEGAL TokenType = "ILLEGAL" // token that didn't define in the language
+	EOF     TokenType = "EOF"
+
+	IDENT  TokenType = "IDENT"  // e.g variable name
+	BILBUL TokenType = "BILBUL" // bilangan bulan / int
+
+	// operator
+	ASSIGN TokenType = "="
+	PLUS   TokenType = "PLUS"
+
+	// delimiter
+	COMMA     TokenType = ","
+	SEMICOLON TokenType = ";"
+
+	// keyword (reserved word specific to the programming language - non user-defined)
+	FUNGSI TokenType = "FUNGSI"
+	BUAT   TokenType = "BUAT"
+)
+
+type Token struct {
+	Type    TokenType
+	Literal string
 }
