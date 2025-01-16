74b96ae788458238557300f26a934604b1680c2f
Vricap <vricagedepenggalih@gmail.com>
2025-01-16 23:59:30 +0700

    eval for integer infix expr done

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -15,6 +15,49 @@ func Eval(node ast.Node) object.Object {
 		return &object.Integer{Value: t.Value}
 	case *ast.BooleanLiteral:
 		return &object.Boolean{Value: t.Value}
+	case *ast.PrefixExpression:
+		right := Eval(t.Right)
+		return evalPrefixExpression(t.Operator, right)
+	case *ast.InfixExpression:
+		left := Eval(t.Left)
+		right := Eval(t.Right)
+		return evalInfixExpression(t.Operator, left, right)
 	}
-	return nil
+	return &object.Nil{}
+}
+
+func evalPrefixExpression(op string, right object.Object) object.Object {
+	switch op {
+	case "!":
+		if right.Inspect() == "benar" {
+			return &object.Boolean{Value: false}
+		} else if right.Inspect() == "salah" {
+			return &object.Boolean{Value: true}
+		} else {
+			return &object.Boolean{Value: false}
+		}
+	case "-":
+		if right.Type() != object.OBJECT_INTEGER {
+			return &object.Nil{}
+		}
+		i := right.(*object.Integer)
+		return &object.Integer{Value: -(i.Value)}
+	}
+	return &object.Nil{}
+}
+
+func evalInfixExpression(op string, left object.Object, right object.Object) object.Object {
+	l := left.(*object.Integer).Value
+	r := right.(*object.Integer).Value
+	switch op {
+	case "+":
+		return &object.Integer{Value: l + r}
+	case "-":
+		return &object.Integer{Value: l - r}
+	case "*":
+		return &object.Integer{Value: l * r}
+	case "/":
+		return &object.Integer{Value: l / r}
+	}
+	return &object.Nil{}
 }
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -16,13 +16,22 @@ func testVal(in string) object.Object {
 	return Eval(tree)
 }
 
-func TestEvalIntegerExpression(t *testing.T) {
+func TestIntegerExpression(t *testing.T) {
 	test := []struct {
 		in     string
 		expect int
 	}{
 		{"4", 4},
 		{"12", 12},
+		{"-5", -5},
+		{"5 + 5 + 5 + 5 - 10", 10},
+		{"2 * 2 * 2 * 2 * 2", 32},
+		{"-50 + 100 + -50", 0},
+		{"5 * 2 + 10", 20},
+		{"5 + 2 * 10", 25},
+		{"20 + 2 * -10", 0},
+		{"50 / 2 * 2 + 10", 60},
+		{"3 * 3 * 3 + 10", 37},
 	}
 
 	for _, tt := range test {
@@ -31,7 +40,7 @@ func TestEvalIntegerExpression(t *testing.T) {
 	}
 }
 
-func TestEvalBoolean(t *testing.T) {
+func TestBoolean(t *testing.T) {
 	test := []struct {
 		in     string
 		expect bool
@@ -46,10 +55,29 @@ func TestEvalBoolean(t *testing.T) {
 	}
 }
 
+func TestBangOperator(t *testing.T) {
+	test := []struct {
+		in     string
+		expect bool
+	}{
+		{"!benar", false},
+		{"!salah", true},
+		{"!1", false},
+		{"!!benar", true},
+		{"!!salah", false},
+		{"!!1", true},
+	}
+
+	for _, tt := range test {
+		eval := testVal(tt.in)
+		testBooleanObject(t, eval, tt.expect)
+	}
+}
+
 func testIntegerObject(t *testing.T, eval object.Object, expect int) {
 	i, ok := eval.(*object.Integer)
 	if !ok {
-		t.Fatalf("object is not *object.Integer. got: %T", i)
+		t.Fatalf("object is not *object.Integer. got: %T", eval)
 	}
 	if i.Value != expect {
 		t.Fatalf("i.Value is not %d. got: %d", expect, i.Value)
@@ -59,7 +87,7 @@ func testIntegerObject(t *testing.T, eval object.Object, expect int) {
 func testBooleanObject(t *testing.T, eval object.Object, expect bool) {
 	b, ok := eval.(*object.Boolean)
 	if !ok {
-		t.Fatalf("object is not *object.Boolean.got: %T", b)
+		t.Fatalf("object is not *object.Boolean.got: %T", eval)
 	}
 	if b.Value != expect {
 		t.Fatalf("i.Value is not %t. got: %t", expect, b.Value)
